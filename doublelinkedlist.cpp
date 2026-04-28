7a5feee1de369a9c9813534c97e376d66583d2f5
bagasadit123 <bagasadit430@gmail.com>
2026-04-28 15:23:18 +0700

    last step 7

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -51,6 +51,9 @@ public:
 
             // Step 6: newNode.prev = NULL
             newNode->prev = NULL;
+                   // Step 7: START = newNode
+            START = newNode;
+            return;
         }
     }
 }
